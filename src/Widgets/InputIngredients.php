416acdd5aab0a7cc6b05ee4a91626dfd34ba50a6
Sascha Wustmann <hi@saschawustmann.com>
2025-11-23 01:22:30 +0100

    Fix ingredient widget

--- src/Widgets/InputIngredients.php
+++ src/Widgets/InputIngredients.php
@@ -24,12 +24,12 @@ class InputIngredients extends Widget
         // Make sure there is at least an empty array
         if (empty($this->value) || !\is_array($this->value)) {
             $this->value = [[
-                0 => '',
-                1 => '',
-                2 => ''
+                'amount' => '',
+                'unit' => '',
+                'ingredient' => ''
             ]];
         }
-        if (!$this->class) $this->class = 'tl_tablewizard';
+        if (!$this->class) $this->class = 'tl_listwizard';
 
         $return = $this->setTableHeaderString($this->id, $this->class);
 
@@ -44,25 +44,42 @@ class InputIngredients extends Widget
 
     public function setTableHeaderString($id, $class): string
     {
-        return '<div id="inputIngredient"><table id="ctrl_' . $id . '" class="' . $class . '"><thead><tr><th class="recipe_amount">' . $GLOBALS['TL_LANG']['tl_recipe']['headers']['amount'] . '</th><th class="recipe_unit">' . $GLOBALS['TL_LANG']['tl_recipe']['headers']['unit'] . '</th><th class="recipe_ingredient">' . $GLOBALS['TL_LANG']['tl_recipe']['headers']['ingredient'] . '</th><th class="recipe_buttons"></th></tr></thead><tbody class="sortable">';
+        return '<div class="' . $class . '" id="inputIngredient">
+                <table data-controller="contao--row-wizard" id="ctrl_' . $id . '">
+                <thead>
+                <tr>
+                <th class="recipe_drag"></th>
+                <th class="recipe_amount">' . $GLOBALS['TL_LANG']['tl_recipe']['headers']['amount'] . '</th>
+                <th class="recipe_unit">' . $GLOBALS['TL_LANG']['tl_recipe']['headers']['unit'] . '</th>
+                <th class="recipe_ingredient">' . $GLOBALS['TL_LANG']['tl_recipe']['headers']['ingredient'] . '</th>
+                <th class="recipe_buttons"></th>
+                </tr>
+                </thead>
+                <tbody class="sortable" data-controller="contao--sortable" data-contao--sortable-handle-value=".drag-handle" data-action="contao--sortable:update->contao--row-wizard#updateSorting" data-contao--row-wizard-target="body">';
     }
 
     public function setTableRowString($id, $value, $name, $i, $arrUnits, $arrIngredients, $arrButtons): string
     {
-        return '<tr><td class="recipe_amount">' . $this->setAmountInputString($id, $value, $name, $i) . '</td><td class="recipe_unit">' . $this->setUnitSelectString($id, $value, $name, $i, $arrUnits) . '</td><td class="recipe_ingredient">' . $this->setIngredientSelectString($id, $value, $name, $i, $arrIngredients) . '</td><td class="recipe_buttons">' . $this->setButtonString($arrButtons) . '</td></tr>';
+        return '<tr data-contao--row-wizard-target="row">
+                <td class="recipe_drag"><button type="button" class="drag-handle" title="Move the item via drag and drop" aria-hidden="true" data-action="keydown-&gt;contao--sortable#move" data-contao--tooltips-target="tooltip"><img src="/system/themes/flexible/icons/drag--dark.svg" width="16" height="16" alt="" class="color-scheme--dark" loading="lazy"><img src="/system/themes/flexible/icons/drag.svg" width="16" height="16" alt="" class="color-scheme--light" loading="lazy"></button></td>
+                <td class="recipe_amount">' . $this->setAmountInputString($id, $value, $name, $i) . '</td>
+                <td class="recipe_unit">' . $this->setUnitSelectString($id, $value, $name, $i, $arrUnits) . '</td>
+                <td class="recipe_ingredient">' . $this->setIngredientSelectString($id, $value, $name, $i, $arrIngredients) . '</td>
+                <td class="recipe_buttons">' . $this->setButtonString($arrButtons) . '</td>
+                </tr>';
     }
 
     public function setAmountInputString($id, $currentValue, $name, $i): string
     {
-        return '<input id="' . $id . '_amount_' . $i . '" type="text" name="' . $id . '[' . $i . '][0]" class="tl_text" value="' . StringUtil::specialchars($currentValue[0]) . '">';
+        return '<input id="' . $id . '_amount_' . $i . '" type="text" name="' . $id . '[' . $i . '][amount]" class="tl_text" value="' . StringUtil::specialchars($currentValue['amount']) . '">';
     }
 
     public function setUnitSelectString($id, $currentValue, $name, $i, $arrUnits): string
     {
-        $unitSelect = '<select id="' . $id . '_unit_' . $i . ' " name="' . $id . '[' . $i . '][1]" class="unit_select tl_select tl_chosen" onfocus="Backend.getScrollOffset()">';
-        ($currentValue[1] == '') ? $unitSelect .= '<option value="" selected>-</option>' : $unitSelect .= '<option value="">-</option>';
+        $unitSelect = '<select id="' . $id . '_unit_' . $i . ' " name="' . $id . '[' . $i . '][unit]" class="unit_select tl_select tl_chosen" onfocus="Backend.getScrollOffset()">';
+        ($currentValue['unit'] == '') ? $unitSelect .= '<option value="" selected>-</option>' : $unitSelect .= '<option value="">-</option>';
         foreach ($arrUnits as $key => $value) {
-            ($currentValue[1] == $key) ? $unitSelect .= '<option value="' . $key . '" selected>' . $value . '</option>' : $unitSelect .= '<option value="' . $key . '">' . $value . '</option>';
+            ($currentValue['unit'] == $key) ? $unitSelect .= '<option value="' . $key . '" selected>' . $value . '</option>' : $unitSelect .= '<option value="' . $key . '">' . $value . '</option>';
         }
         $unitSelect .= '</select>';
         return $unitSelect;
@@ -70,10 +87,10 @@ class InputIngredients extends Widget
 
     public function setIngredientSelectString($id, $currentValue, $name, $i, $arrIngredients): string
     {
-        $ingredientSelect = '<select id="' . $id . '_ingredient_' . $i . '" name="' . $id . '[' . $i . '][2]" class="ingredient_select tl_select tl_chosen" onfocus="Backend.getScrollOffset()">';
-        ($currentValue[2] == '') ? $ingredientSelect .= '<option value="" selected>-</option>' : $ingredientSelect .= '<option value="">-</option>';
+        $ingredientSelect = '<select id="' . $id . '_ingredient_' . $i . '" name="' . $id . '[' . $i . '][ingredient]" class="ingredient_select tl_select tl_chosen" onfocus="Backend.getScrollOffset()">';
+        ($currentValue['ingredient'] == '') ? $ingredientSelect .= '<option value="" selected>-</option>' : $ingredientSelect .= '<option value="">-</option>';
         foreach ($arrIngredients as $key => $value) {
-            ($currentValue[2] == $key) ? $ingredientSelect .= '<option value="' . $key . '" selected>' . $value . '</option>' : $ingredientSelect .= '<option value="' . $key . '">' . $value . '</option>';
+            ($currentValue['ingredient'] == $key) ? $ingredientSelect .= '<option value="' . $key . '" selected>' . $value . '</option>' : $ingredientSelect .= '<option value="' . $key . '">' . $value . '</option>';
         }
         $ingredientSelect .= '</select>';
         return $ingredientSelect;
@@ -81,15 +98,8 @@ class InputIngredients extends Widget
 
     public function setButtonString($arrButtons): string
     {
-        $buttons = '';
-        foreach ($arrButtons as $button) {
-            if ($button == 'rdrag') {
-                $buttons .= ' <button type="button" class="drag-handle" title="' . StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['move']) . '" aria-hidden="true">' . Image::getHtml('drag.svg') . '</button>';
-            } else {
-                $buttons .= ' <button type="button" class="tl_tablewizard_img" data-command="' . $button . '" title="' . StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['lw_' . substr($button, 1)]) . '">' . Image::getHtml(substr($button, 1) . '.svg') . '</button>';
-            }
-        }
-        return $buttons;
+        return '<button type="button" data-action="contao--row-wizard#copy contao--scroll-offset#store" data-command="copy" title="Duplicate the row" data-contao--tooltips-target="tooltip"><img src="/system/themes/flexible/icons/copy.svg" width="16" height="16" alt=""></button>
+                <button type="button" data-action="contao--row-wizard#delete contao--scroll-offset#store" data-command="delete" title="Delete the row" data-contao--tooltips-target="tooltip"><img src="/system/themes/flexible/icons/delete.svg" width="16" height="16" alt=""></button>';
     }
 
     public function setTableFooterString($id): string
