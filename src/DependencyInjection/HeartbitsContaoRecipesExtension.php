ea3d3de563615d74ac0ea1a81ba28d9e24351dd7
Sascha Wustmann <hi@saschawustmann.com>
2026-07-02 13:27:37 +0200

    Include rating stars dynamically

--- src/DependencyInjection/HeartbitsContaoRecipesExtension.php
+++ src/DependencyInjection/HeartbitsContaoRecipesExtension.php
@@ -6,9 +6,19 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
-class HeartbitsContaoRecipesExtension extends Extension
+class HeartbitsContaoRecipesExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('twig', [
+            'paths' => [
+                '%kernel.project_dir%/public/bundles/heartbitscontaorecipes' => 'heartbitscontaorecipes',
+            ],
+        ]);
+    }
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader(
